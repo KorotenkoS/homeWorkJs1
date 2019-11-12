80cbb9af134e920aa6f26d73bba098df51090201
KorotenkoS <sergey588@ua.fm>
2019-11-12 12:07:26 +0200

    alert po knopkу mirik

--- main.js
+++ main.js
@@ -7,15 +7,8 @@ var container = document.querySelector('#list-names');
 var html = '';
 
 for(var name of names){
-        html += '<li>' + name + '</li>'
+    html += `<li onclick="alert(Вы выбрали \${this.innerText})">${name}</li>`
 };
 console.log(html);
 
-container.innerHTML = html;  
-
-function alert(names) { 
-    for(var name of names)
-    alert("вы выбрали имя" + name);
-    
-}
-name.onclick = alert;
+container.innerHTML = html;
